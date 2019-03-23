6d5faaf6e4c76cf211d59978494cdfc9a2fbc4b7
Gagan <skielight16@gmail.com>
2019-03-22 23:06:31 -0400

    adding homework; deleting boxit extra files

--- box_it_script/lab2day14.sql
+++
@@ -1,6 +0,0 @@
-select p.name, o.complete_on
-    from products as p
-    inner join line_items as li ON li.product_id = p.id 
-    inner join orders as o on li.order_id = o.id
-    where o.completed_on > current_date - interval '24 month'
-    order by o.completed_on DESC;
